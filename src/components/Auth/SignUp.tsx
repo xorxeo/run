ac6124867ee488aec76cea4c590e1a5334ec63fe
xorxeo <86877609+xorxeo@users.noreply.github.com>
2023-02-28 20:28:24 +0300

    Improve/react hook form (#3)
    Change native form to React Hook Form on signup/signin pages #1

--- src/components/Auth/SignUp.tsx
+++ src/components/Auth/SignUp.tsx
@@ -1,100 +1,84 @@
 // import "Auth.module.css"
-
-import { useAuth, authUserContext } from "@/containers/AuthUserContainer";
+import { useAuth } from "@/containers/AuthUserContainer";
+import { FirebaseError } from "firebase/app";
 import { useRouter } from "next/router";
-import { useContext, useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { FirebaseErrorCodes } from "./auth.types";
+
+type Inputs = {
+  email: string;
+  password: string;
+};
 
 export const SignUp = () => {
-  const [email, setEmail] = useState("");
-  const [passwordOne, setPasswordOne] = useState("");
-  const [passwordTwo, setPasswordTwo] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm<Inputs>();
+
   const router = useRouter();
-  const [error, setError] = useState<string | null>(null);
 
   const { createUserEmailAndPassword } = useAuth();
 
-  const q = useContext(authUserContext);
+  const onSubmit = handleSubmit(async ({ email, password }) => {
+    try {
+      await createUserEmailAndPassword(email, password);
+      router.push("/");
+    } catch (error) {
+      const { code } = error as FirebaseError;
 
-  const onSubmit = (e: React.FormEvent) => {
-    setError(null);
-    if (passwordOne === passwordTwo) {
-
-      createUserEmailAndPassword(email, passwordOne)
-        .then((authUser) => {
-          console.log("Success");
-          router.push("/");
-          console.log(q.authUser?.email);
-        })
-        .catch((error) => {
-          setError(error.message);
-        });
-    } else {
-      setError("Password do not match");
+      if (code === FirebaseErrorCodes.WEAK_PASSWORD) {
+        setError("password", { message: code });
+        return;
+      }
+      if (code === FirebaseErrorCodes.INVALID_EMAIL) {
+        setError("email", { message: code });
+        return;
+      }
+      if (code === FirebaseErrorCodes.EMAIL_EXISTS) {
+        setError("email", { message: code });
+        return;
+      }
     }
-    e.preventDefault();
-  };
+  });
 
   return (
-    <div
-      className="flex h-screen w-screen bg-slate-100  "
-    >
+    <div className="flex h-screen w-screen bg-slate-100  ">
       <div className="flex flex-col m-auto bg-slate-200  h-[60%] w-[80%] hero-content shadow-md rounded-md">
         <form onSubmit={onSubmit} className="flex flex-col w-[40%]">
           <h1 className="flex  w-[100%] text-xl font-normal h-12">Register</h1>
 
           <div className="form-control font-light">
-            <label className="label pl-0">
+            <label className="label pl-0 h-12">
               <span className="label-text">Email address</span>
             </label>
             <input
               type="email"
-              value={email}
-              onChange={(event) => setEmail(event.target.value)}
-              name="email"
-              id="signUpEmail"
               autoComplete="on"
               placeholder="email"
               className="input input-bordered"
+              {...register("email", { required: true })}
             />
           </div>
+          {errors.email && <div>{errors.email.message}</div>}
 
           <div className="form-control font-light">
-            <label className="label pl-0">
+            <label className="label pl-0 h-12">
               <span className="label-text">Password</span>
             </label>
             <input
               type="password"
-              name="passwordOne"
-              value={passwordOne}
-              onChange={(event) => setPasswordOne(event.target.value)}
-              id="signUpPassword1"
               autoComplete="on"
               placeholder="password"
               className="input input-bordered"
+              {...register("password", { required: true })}
             />
           </div>
+          {errors.password && <div>{errors.password.message}</div>}
 
-          <div className="form-control font-light">
-            <label className="label pl-0">
-              <span className="label-text">Repeat password</span>
-            </label>
-            <input
-              type="password"
-              name="passwordOne"
-              value={passwordTwo}
-              onChange={(event) => setPasswordTwo(event.target.value)}
-              id="signUpPassword2"
-              autoComplete="on"
-              placeholder="password"
-              className="input input-bordered"
-            />
-            <div className="label flex justify-between h-12">
-              <a href="#" className="label-text-alt link link-hover">
-                Forgot password?
-              </a>
-            </div>
-          </div>
-          <div className="flex justify-center h-12 rounded-lg  bg-[#FBBD23] font-normal hover:scale-105 transition-transform duration-200 ">
+          <div className="flex justify-center h-12 rounded-lg mt-12 bg-[#FBBD23] font-normal hover:scale-105 transition-transform duration-200 ">
             <button>Sign up</button>
           </div>
         </form>
---
+++ src/components/Auth/auth.types.ts
@@ -0,0 +1,7 @@
+export enum FirebaseErrorCodes {
+  EMAIL_NOT_FOUND = "auth/user-not-found",
+  INVALID_PASSWORD = "auth/wrong-password",
+  WEAK_PASSWORD = "auth/weak-password",
+  INVALID_EMAIL = "auth/invalid-email",
+  EMAIL_EXISTS = "auth/email-already-in-use",
+}
